a108f89198082269191ce2c9a0bd7b0702dbe34a
Oscar Ramos <oscarjrs7@gmail.com>
2022-01-19 12:44:19 -0500

    using an adapter for fetchign

--- src/adapters/dataAdapter.js
+++ src/adapters/dataAdapter.js
@@ -18,8 +18,6 @@ function GetData()
         const response = await fetch("http://localhost:3000/data");
         const tempData = await response.json();
         console.log("Fetched Data...")
-        // setTemperature(tempData.temperature);
-        // setHumidity(tempData.humidity);
         updateData(tempData);
         console.log("Temperature: " + tempData.temperature);
         console.log("Humidity: " + tempData.humidity);
